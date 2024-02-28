31319263de19f0485154797eb6ce179dc5b8c42b
shekhar <shekharsutharsirohi@gmail.com>
2024-02-28 04:44:09 +0000

    new

---
+++ server.js
@@ -0,0 +1,18 @@
+// Import required modules
+const express = require('express');
+
+// Create an instance of Express
+const app = express();
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+// Define port
+const PORT = process.env.PORT || 3000;
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
